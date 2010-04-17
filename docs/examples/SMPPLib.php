70f322b52ddcbabc1baa5ba300f3032d4d86a441
Simon Spencer <silospen@php.net>
2010-04-17 09:00:13 +0000

    Added header blocks
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_SMPP/trunk@298113 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/SMPPLib.php
+++ docs/examples/SMPPLib.php
@@ -1,4 +1,31 @@
 <?php
+
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * SMPP v3.4 SMPPLib class
+ *
+ * PHP versions 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Networking
+ * @package    Net_SMPP
+ * @author     Silospen <silospen@silospen.com>
+ * @copyright  Portions of the documentation (c) Copyright 1999 SMPP Developers
+ *             Forum.
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    SVN: $Id$
+ * @since      Release
+ * @link       http://pear.php.net/package/Net_SMPP
+ */
+
+// Place includes, constant defines and $_GLOBAL settings here.
 error_reporting(E_ALL);
 require_once 'Net/Socket.php'; 
 require_once 'Net/SMPP.php';
@@ -37,6 +64,23 @@ define('SMPP_RECEIVER', 'bind_receiver');
  * The size of the sending stack, defined in the SMPP docs
  */
 define('MAX_STACK_SIZE', 10);
+
+/**
+ * SMPPLib class
+ *
+ * This is an example of a client set up to use Net_SMPP.
+ *
+ * @category   Networking
+ * @package    Net_SMPP
+ * @author     Silospen <silospen@silospen.com>
+ * @copyright  Portions of the documentation (c) Copyright 1999 SMPP Developers
+ *             Forum.
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    Release: @package_version@
+ * @since      Release
+ * @link       http://pear.php.net/package/Net_SMPP
+ */
+
 
 class SMPPLib
 {
